bc97642d76688975b59ef31b05a5d9de1dedb09e
Leonardo Candido Czaplicki <leocandido562@gmail.com>
2022-08-23 10:48:31 -0300

    terminei a aula15 e até o 7 no youtube

---
+++ jsyoutube/4_variaveis/js/scripts.js
@@ -0,0 +1,13 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
---
+++ jsyoutube/6_string/js/scripts.js
@@ -0,0 +1,11 @@
+var nome = "Matheus";
+var sobrenome = "Battisti";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+document.write("Olá!");
