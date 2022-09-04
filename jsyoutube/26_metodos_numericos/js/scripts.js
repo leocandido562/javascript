61983e86f5309c2c99b782923dd90682352d1826
Leonardo Candido Czaplicki <leocandido562@gmail.com>
2022-09-03 21:30:28 -0300

    aulas

---
+++ jsyoutube/26_metodos_numericos/js/scripts.js
@@ -0,0 +1,20 @@
+// parseFloat
+
+console.log(parseFloat('12.66'));
+
+// parseInt
+
+console.log(parseInt(12.66));
+
+// toFixed
+
+console.log(12.66.toFixed(1));
+
+// isNaN
+
+console.log(isNaN(12.66));
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
---
+++ jsyoutube/27_metodos_de_string/js/scripts.js
@@ -0,0 +1,18 @@
+// toLowerCase() e toUpperCase()
+
+var frase = "Esta é uma frase que vamos manipular";
+
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+
+// trim()
+
+var nome = "             Leonardo      ";
+
+var nomeTrim = nome.trim();
+
+console.log(nomeTrim);
+
+// split()
+
+console.log(frase.split(" "));
