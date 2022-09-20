ccd9b9b19c204327fa1bad284db19df6be302987
Leonardo Candido Czaplicki <leocandido562@gmail.com>
2022-09-20 14:43:26 -0300

    aulas

---
+++ jsyoutube/40_adicionar_alterar_css/js/scripts.js
@@ -0,0 +1,12 @@
+// adiciona elemento
+var title = document.querySelector("#title");
+
+// adiciona o estilo
+title.style.color = "red";
+
+// background-color
+title.style.backgroundColor = "gray";
+
+// adicionar vários estilos
+var subtitle = document.querySelector(".subtitle");
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px;";
---
+++ jsyoutube/44_onload/js/scripts.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+
+    console.log("Carregou o DOM");
+
+}
