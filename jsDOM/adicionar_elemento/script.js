4fa3fd065cdd661c02cffb195c5b47c718ae1519
Leonardo Candido Czaplicki <leocandido562@gmail.com>
2022-10-16 17:54:15 -0300

    aulasDom

---
+++ jsDOM/adicionar_elemento/script.js
@@ -0,0 +1,9 @@
+// Criar elemento, colocar classe e valor
+let newElement = document.createElement("p");
+newElement.className = "vermelho" ;
+newElement.innerText = "Novo elemento";
+
+// Adicionar elemento na div e clonar
+let mydiv = document.querySelector("div");
+mydiv.appendChild(newElement);
+mydiv.appendChild(newElement.cloneNode(true));
